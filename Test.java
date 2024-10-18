abe06de51ddd46d62ca59c840370047b1df0e36c
HussainAbedin <hussainabedin54@gmail.com>
2024-10-18 15:28:43 +0500

    Even Number Checking Program

--- Test.java
+++ Test.java
@@ -3,5 +3,12 @@ class Test
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+		int n=4;
+		if(n%2==0)
+		System.out.println("EVEN");
+		else
+		System.out.println("ODD");
+		
+
 	}
 }
